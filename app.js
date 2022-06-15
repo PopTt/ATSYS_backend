24b76382901ee9fe6e62af335ef027b2695ad018
LJX <jingxiang1319@gmail.com>
2022-06-15 18:19:39 +0800

    CORS stuff setup

--- app.js
+++ app.js
@@ -1,29 +1,45 @@
-const express = require("express")
-const cors = require("cors")
-const app = express()
-const user_router = require('./api/routers/user-router')
-const event_router = require('./api/routers/event-router')
-const attendance_router = require('./api/routers/attendance-router')
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const user_router = require('./api/routers/user-router');
+const event_router = require('./api/routers/event-router');
+const attendance_router = require('./api/routers/attendance-router');
 
-const dotenv = require('dotenv')
-dotenv.config({ path: './.env'})
-const PORT = process.env.PORT || 5000
+const dotenv = require('dotenv');
+dotenv.config({ path: './.env' });
+const PORT = process.env.PORT || 5000;
 
-app.use(cors())
+//CORS Settings
+const corsOption = {
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+};
+app.use(cors(corsOption));
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+//Incoming Request Settings
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ extended: true }));
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', process.env.FRONTEND_URL);
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization,  X-PINGOTHER'
+  );
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS'
+  );
+  next();
+});
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/users', user_router)
-app.use('/events', event_router)
-app.use('/attendances', attendance_router)
+app.use('/users', user_router);
+app.use('/events', event_router);
+app.use('/attendances', attendance_router);
 
 app.listen(PORT, () => {
-    console.log("Server started on Port " + PORT)
-})
+  console.log('Server started on Port ' + PORT);
+});
