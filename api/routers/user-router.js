a195df29b0490bc22598aa77013d3db76333de23
LJX <jingxiang1319@gmail.com>
2022-06-15 18:20:38 +0800

    code structure pretty update

--- api/routers/user-router.js
+++ api/routers/user-router.js
@@ -1,20 +1,19 @@
-const user_controller = require('../controllers/user-controller')
-const app = require('express')
+const user_controller = require('../controllers/user-controller');
+const app = require('express');
 
-const router = app.Router()
-const ROLES = require('../../config/roles')
-const { checkWebToken } = require('../../middlewares-auth/verify-jwt')
-const checkRoles = require('../../middlewares-auth/verify-role')
+const router = app.Router();
+const ROLES = require('../../config/roles');
+const { checkWebToken } = require('../../middlewares-auth/verify-jwt');
+const checkRoles = require('../../middlewares-auth/verify-role');
 
+router.get('/', checkWebToken, user_controller.getUser);
+router.get('/:user_id', checkWebToken, user_controller.getUserByUID);
 
-router.get('/', checkWebToken, user_controller.getUser)
-router.get('/:user_id', checkWebToken, user_controller.getUserByUID)
+router.patch('/', checkWebToken, user_controller.updateUser);
+router.delete('/', checkWebToken, user_controller.deleteUser);
 
-router.patch('/', checkWebToken, user_controller.updateUser)
-router.delete('/', checkWebToken, user_controller.deleteUser)
+router.post('/register', user_controller.register);
+router.post('/logout', checkWebToken, user_controller.logout);
+router.post('/login', user_controller.login);
 
-router.post('/register', user_controller.register)
-router.post('/logout', checkWebToken, user_controller.logout)
-router.post('/login', user_controller.login)
-
-module.exports = router
+module.exports = router;
